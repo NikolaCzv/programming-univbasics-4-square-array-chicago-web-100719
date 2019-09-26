329ff6f78990fdcdbd2392362e47c378efb7d41c
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-26 21:36:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
  counter = 0
- while counter < square_array.  do 
+ while counter < square_array.count  do 
    puts square_array[counter] *= square_array[counter]
    counter += 1
 end
