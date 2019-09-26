f0a58719790fe2e5cdd58bac820020b94e742457
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-26 21:39:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  squered = []
  counter = 0
  
- while counter < square_array.count  do 
+ while counter < array.count  do 
    puts square_array[counter] *= square_array[counter]
    counter += 1
 end
